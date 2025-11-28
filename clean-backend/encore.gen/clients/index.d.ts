821f270adb3d5f1d4bc4247b6f8cbca2434cd2d1
Toure Young <ecrofmedia@gmail.com>
2025-11-28 18:58:16 -0500

    deploy clean backend to encore

---
+++ clean-backend/encore.gen/clients/index.d.ts
@@ -0,0 +1,2 @@
+export * as auth from "../internal/clients/auth/endpoints";
+export * as onboarding from "../internal/clients/onboarding/endpoints";
---
+++ clean-backend/encore.gen/clients/index.js
@@ -0,0 +1,2 @@
+export * as auth from "../internal/clients/auth/endpoints.js";
+export * as onboarding from "../internal/clients/onboarding/endpoints.js";
