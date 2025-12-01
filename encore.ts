9b9a6a1983e115bf1f21121ff0c60e6eb84c6097
Toure Young <ecrofmedia@gmail.com>
2025-11-30 20:18:36 -0500

    trigger deploy

---
+++ encore.ts
@@ -0,0 +1,10 @@
+// Root entrypoint for Encore TypeScript apps
+
+// Auth service
+import "./auth/encore.service";
+
+// Onboarding service
+import "./onboarding/encore.service";
+
+console.log("Encore backend initialized.");
+
